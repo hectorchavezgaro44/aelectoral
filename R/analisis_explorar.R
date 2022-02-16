a060bf25040be2f5c1a7bea02cda6886c768993e
hectorchavezgaro44 <hector.chaga94@gmail.com>
2022-02-16 09:19:55 -0600

    agrega funciones nuevas

--- R/analisis_explorar.R
+++ R/analisis_explorar.R
@@ -360,3 +360,46 @@ probar_independencia_ganador <-function(bd, ganador, eleccion, ...){
   return(res)
 }
 
+
+#' Title
+#'
+#' @param bd
+#' @param eleccion
+#' @param colores_nombrados
+#' @param grupo
+#' @param saturacion
+#'
+#' @return
+#' @export
+#'
+#' @examples
+colorear_ganador_degradado <- function(bd,eleccion, colores_nombrados, grupo, saturacion=.9){
+  # Partidos
+  partidos <- names(colores_nombrados)
+
+  # Homologar colores
+  colores_saturados <- colores_nombrados %>%  shades::saturation(saturacion)
+  names(colores_saturados) <- names(colores_nombrados)
+
+  # Calcular ganador y máximo de votación
+  bd <- bd %>%
+    select({{grupo}},matches(glue::glue("ele_{partidos}_{eleccion}"))) %>% na.omit() %>%
+    rowwise()
+  bd <- bd %>%
+    mutate(ganador=list(which.max(c_across(cols = starts_with("ele_")))) %>%
+             map_chr(~names(bd)[[.x+1]]) ,
+           max_votacion=max(c_across(cols = starts_with("ele_")))
+    ) %>%
+    ungroup() %>%
+    mutate(ganador= stringr::str_remove(ganador, "ele_") %>%
+             stringr::str_remove(., pattern = glue::glue("_{eleccion}")))
+  # Funciones de color
+  funciones_color <- map(unique(bd$ganador),
+                         ~colorRamp(colors = c("white",colores_saturados[[.x]]), space = "Lab") %>%
+                           leaflet::colorNumeric(domain = c(0, max(bd$max_votacion))))
+  names(funciones_color) <- unique(bd$ganador)
+
+  res <- bd %>% mutate(color_ganador=map2_chr(ganador, max_votacion,~funciones_color[[.x]](.y)))
+  return(res)
+}
+
